fc89b30dbaed6d857b3541d81443760ca9a3adcb
Gbenga19 <61985187+Gbenga19@users.noreply.github.com>
2022-06-07 14:10:47 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+
+#RULES
+'''
+Rock beat Scissors
+Scissors beat Paper
+Paper beat Rock
+'''
+
+while True:
+    options = ['Rock', 'Scissors', 'Paper']
+    computer = random.choice(options)
+    player = None
+    
+    while player not in options:
+        print("Invalid Option")
+        pass
+        player = input("Enter an option -- Rock, Scissors or Paper: ")
+           
+    if player == computer:
+        print(f"Computer {computer} : player {player}")
+        print('Tie, Play again')
+        player = input("Enter an option -- Rock, Scissors or Paper: ")
+
+    elif player == 'Rock' and computer == 'Scissors' or player =='Scissors' and computer == 'Paper' or player == 'Paper' and computer == 'Rock':
+        print(f"Player ({player}) : CPU ({computer})")
+        print("You win")
+        break
+    
+    elif player == 'Scissors' and computer == 'Rock' or player =='Paper' and computer == 'Scissors' or player == 'Rock' and computer == 'Paper':
+        print(f" Player ({player}) : CPU ({computer})")
+        print("Computer win")
+        break
+
+
+
+
+        
+
